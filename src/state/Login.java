9c48a18bafba6e2aba192f012f3a4b469615508f
Grant Upson <gupson@utas.edu.au>
2020-04-15 16:33:20 +1000

    Tidied up the different states and fixed interaction bugs.

--- src/state/Login.java
+++ src/state/Login.java
@@ -20,7 +20,7 @@ public class Login implements State
     {
         this.connection = connection;
         this.writer = writer;
-        accountDatabase = new Database("grant", "Killthemall2");
+        accountDatabase = new Database("USER", "PASS");
     }
 
     @Override
