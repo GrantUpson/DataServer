98c91ce1854384f28a664508f74b9dcca31e4758
Grant Upson <gupson@utas.edu.au>
2020-05-02 14:22:24 +1000

    VDV

--- src/state/Register.java
+++ src/state/Register.java
@@ -21,7 +21,7 @@ public class Register implements State
     {
         this.connection = connection;
         this.writer = writer;
-        this.accountDatabase = new Database("grant", "Killthemall2");
+        this.accountDatabase = new Database("root", "");
     }
 
     @Override
